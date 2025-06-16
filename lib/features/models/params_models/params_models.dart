7e027bdef557b6cb0c296db908de390a31c287a9
tanvirhridoy <ikramulhasantanvir@gmail.com>
2025-06-16 22:29:26 +0600

    .....

--- lib/features/models/params_models/params_models.dart
+++ lib/features/models/params_models/params_models.dart
@@ -2,11 +2,11 @@ class ParamsModels{
   final String name;
   final String email;
   final String number;
-  final String genger;
+  final String gender;
   final String birthday;
   final String height;
   final String weight;
   final String password;
 
-  ParamsModels({required this.name, required this.email, required this.number, required this.genger, required this.birthday, required this.height, required this.weight, required this.password});
+  ParamsModels({required this.name, required this.email, required this.number, required this.gender, required this.birthday, required this.height, required this.weight, required this.password});
 }
