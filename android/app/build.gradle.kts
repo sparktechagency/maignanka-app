dccde25110be010f22543eec0709289fbf6ca356
tanvir-hridoy <ikramulhasantanvir@gmail.com>
2025-11-11 17:40:40 +0600

    ..............

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
         applicationId = "com.kisdates.maignanka_app"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 4
+        versionCode = 5
         versionName = "1.0.0"
     }
 
